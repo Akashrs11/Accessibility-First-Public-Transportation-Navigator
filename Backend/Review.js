f84d3ccc7176c820f6ad7544ebd9c82f7290f181
Akash Shedabal <145891377+Akashrs11@users.noreply.github.com>
2025-05-21 19:22:11 +0530

    Delete Backend directory

--- Backend/Review.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
-  name: String,
-  message: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('Review', reviewSchema);
